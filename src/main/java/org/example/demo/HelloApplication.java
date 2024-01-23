3cffb47f44cbfdeb08ea8c9717107c073d17e1fe
iratherfear <69635438+iratherfear@users.noreply.github.com>
2024-01-23 15:59:31 +0530

    Created the same app using code instead of fxml

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -2,7 +2,10 @@ package org.example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,12 +13,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("hey there!");
-        stage.setScene(scene);
-        stage.show();
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("hey there!");
+//        stage.setScene(scene);
+//        stage.show();
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
 
+        Label greetings = new Label("Welcome to new app");
+        root.getChildren().add(greetings);
+
+        stage.setTitle("JavaFX");
+        stage.setScene(new Scene(root, 300, 300));
+        stage.show();
     }
 
     public static void main(String[] args) {
