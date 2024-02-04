7319731ea50a3088cd58e62993daeccab0f4fce4
iratherfear <69635438+iratherfear@users.noreply.github.com>
2024-02-04 17:47:43 +0530

    Adding id to each button. Modifying the controller as per the button pressed(identified by event)

--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -2,15 +2,23 @@ package org.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private TextField nameField;
-
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
     @FXML
     protected void onHelloButtonClick(ActionEvent e) {
-        System.out.println("Hello, " + nameField.getText());
-        System.out.println("Button pressed: " + e.getSource());
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        }
+        if(e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }
